b81179578c46d4f047bb57ca07ae552759f0fa92
ShaluPruthi <sahlupruthi26@gmail.com>
2025-03-22 23:59:23 +0530

    custom

--- firstReact/src/main.jsx
+++ firstReact/src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            Learn
+        </div>
+    )
+}
 createRoot(document.getElementById('root')).render(
-    <App />
+    <MyApp />
 )
